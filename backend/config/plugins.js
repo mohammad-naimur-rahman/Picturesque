b56091e3d24b665ec762bdfa95ca33df55e86379
Naimur Rahman <64221853+mohammad-naimur-rahman@users.noreply.github.com>
2022-09-02 23:29:24 +0600

    removed vercel configs

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -13,14 +13,4 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  "vercel-deploy": {
-    enabled: true,
-    config: {
-      deployHook: env("VERCEL_DEPLOY_PLUGIN_HOOK"),
-      apiToken: env("VERCEL_DEPLOY_PLUGIN_API_TOKEN"),
-      appFilter: env("VERCEL_DEPLOY_PLUGIN_APP_FILTER"),
-      teamFilter: env("VERCEL_DEPLOY_PLUGIN_TEAM_FILTER"),
-      roles: ["strapi-super-admin"],
-    },
-  },
 });
