9215769eb3896e8d7eb3fc0926f2dcbd9d4152a9
Naimur <naeemhasan28@gmail.com>
2022-09-03 15:56:12 +0600

    try to fix deploy error

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS", [
+      "dIgGwBcSHCH5zSf2uPy1hg==",
+      "mkx1+rMeQzWoUawvY4z0kA==",
+      "ZlfBm5jBLs0IXoVQP8mNOw==",
+      "8CdiuSPS1qxG/fwHAp6jeA==",
+    ]),
   },
 });
