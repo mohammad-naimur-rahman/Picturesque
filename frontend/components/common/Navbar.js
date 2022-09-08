6f8061d433a5639209285bb0bab1e9a6f62b1362
Naimur Rahman <64221853+mohammad-naimur-rahman@users.noreply.github.com>
2022-09-09 02:00:02 +0600

    changed logo

--- frontend/components/common/Navbar.js
+++ frontend/components/common/Navbar.js
@@ -1,11 +1,19 @@
 import classNames from 'classnames'
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from 'styles/components/common/navbar.module.scss'
 
 const Navbar = () => {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('DOMContentLoaded', () => {
+        console.log('DOM IS READY')
+      })
+    }
+  }, [])
   return (
-    <nav>
+    <nav className='px-7 py-4 bg-primary h-[85px] flex align-middle justify-between text-white'>
+      <img src='/logo.png' alt='Picturesque' className='h-full' />
       <ul>
         <li>fb</li>
         <li>pinterest</li>
