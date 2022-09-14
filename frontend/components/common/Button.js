d651079cf2c2275b988d8fc867ca9c372ccd3b26
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2022-09-14 15:40:10 +0600

    added classNames for the button component

--- frontend/components/common/Button.js
+++ frontend/components/common/Button.js
@@ -1,12 +1,66 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Button = ({ children, white = false, edge = false, solid = false, ...rest }) => {
+const Button = ({ children, white = false, edge = false, solid = false, inverted = false, ...rest }) => {
+  const [btnClassName, setbtnClassName] = useState('')
+
+  useEffect(() => {
+    switch (true) {
+      case white && solid && edge && inverted:
+        setbtnClassName('btn-white-solid-edge-inverted')
+        break
+      case !white && solid && edge && inverted:
+        setbtnClassName('btn-black-solid-edge-inverted')
+        break
+      case white && solid && edge:
+        setbtnClassName('btn-white-solid-edge')
+        break
+      case !white && solid && edge:
+        setbtnClassName('btn-black-solid-edge')
+        break
+      case white && solid && inverted:
+        setbtnClassName('btn-white-solid-inverted')
+        break
+      case !white && solid && inverted:
+        setbtnClassName('btn-black-solid-inverted')
+        break
+      case white && edge && inverted:
+        setbtnClassName('btn-white-edge-inverted')
+        break
+      case !white && edge && inverted:
+        setbtnClassName('btn-black-edge-inverted')
+        break
+      case white && solid:
+        setbtnClassName('btn-white-solid')
+        break
+      case !white && solid:
+        setbtnClassName('btn-black-solid')
+        break
+      case white && edge:
+        setbtnClassName('btn-white-edge')
+        break
+      case !white && edge:
+        setbtnClassName('btn-black-edge')
+        break
+      case white && inverted:
+        setbtnClassName('btn-white-inverted')
+        break
+      case !white && inverted:
+        setbtnClassName('btn-black-inverted')
+        break
+      case white:
+        setbtnClassName('btn-white')
+        break
+      case !white:
+        setbtnClassName('btn-black')
+        break
+      default:
+        setbtnClassName('btn-black')
+    }
+  }, [white, solid, edge, inverted])
+
   return (
-    <button
-      {...rest}
-      className={`btn ${white ? 'btn-white' : 'btn-black'} ${edge ? 'btn-edge' : ''} ${solid ? 'btn-solid' : ''}`}
-    >
+    <button {...rest} className={`btn ${btnClassName}`}>
       {children}
     </button>
   )
@@ -18,5 +72,6 @@ Button.propTypes = {
   children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
   white: PropTypes.bool,
   edge: PropTypes.bool,
-  solid: PropTypes.bool
+  solid: PropTypes.bool,
+  inverted: PropTypes.bool
 }
--- frontend/components/pages/Home/HomeContactIntro.js
+++ frontend/components/pages/Home/HomeContactIntro.js
@@ -4,15 +4,17 @@ import React from 'react'
 const HomeContactIntro = () => {
   return (
     <div className='container mb-20'>
-      <Button edge>Click Me</Button>
-      <Button edge solid>
-        Click Me
-      </Button>
-      <Button white>Click Me</Button>
-      <Button solid white>
-        Click Me
-      </Button>
-      "https://www.npmjs.com/package/simplebar-react"
+      <div className='bg-black h-56'>
+        <Button white>Click Me</Button>
+        <Button white solid>
+          Click Me
+        </Button>
+      </div>
+      <div className='h-56 border'>
+        <Button>Click Me</Button>
+        <Button solid>Click Me</Button>
+      </div>
+      <p>https://www.npmjs.com/package/simplebar-react</p>
     </div>
   )
 }
