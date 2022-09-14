f02bbf18aac326a54664414abe6616ed02aed71c
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2022-09-14 21:12:30 +0600

    finished reponsiveness for the homepage

--- frontend/components/common/Footer.js
+++ frontend/components/common/Footer.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='text-center py-6'>
-      <p className='font-light text-xl'>
+      <p className='font-light text-base md:text-xl'>
         Copyright © {new Date().getFullYear()} All rights reserved to{' '}
         <a href={AUTHOR_URL} target='_blank' rel='noreferrer' className='italic'>
           Mohammad Naimur Rahman
--- frontend/components/pages/Home/HomeContactIntro.js
+++ frontend/components/pages/Home/HomeContactIntro.js
@@ -9,15 +9,15 @@ const HomeContactIntro = ({ bgText5, contactTitle }) => {
   return (
     <div className={styles['home-container-fluid']}>
       <p className={classNames(styles['big-text'], styles['big-text-5'])}>{bgText5.data.attributes.title}</p>
-      <div className='container text-center py-14 z-10'>
+      <div className='container text-center py-8 md:py-14 z-10'>
         <h1 className='text-[30px] md:text-[45px] lg:text-[50px] xl:text-[55px] font-extralight'>
           {contactTitle.data.attributes.title}
         </h1>
-        <h2 className='text-[30px] md:text-[45px] lg:text-[50px] xl:text-[55px] font-light mt-8'>
+        <h2 className='text-[30px] md:text-[45px] lg:text-[50px] xl:text-[55px] font-light mt-4 md:mt-8'>
           {contactTitle.data.attributes.description}
         </h2>
         <Link href='/contact'>
-          <Button className='my-20'>Contact Us</Button>
+          <Button className='my-8 md:my-10 lg:my-20'>Contact Us</Button>
         </Link>
       </div>
     </div>
