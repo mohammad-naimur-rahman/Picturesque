84edbcb0e40595aedea92fa7a22fbe07efbacac5
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2022-09-20 10:05:16 +0600

    added query client for tanstack query

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,10 +1,19 @@
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import '../styles/globals.scss'
 import 'tippy.js/dist/tippy.css'
 import 'owl.carousel/dist/assets/owl.carousel.css'
 import 'owl.carousel/dist/assets/owl.theme.default.css'
+import { useState } from 'react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(() => new QueryClient())
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
