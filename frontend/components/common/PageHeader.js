cee8f1949b43337665cf596e4807099b4c706976
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2022-09-24 00:40:34 +0600

    completed page header

--- frontend/components/common/PageHeader.js
+++ frontend/components/common/PageHeader.js
@@ -1,18 +1,47 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Div100vh from 'react-div-100vh'
 import styles from 'styles/components/common/page-header.module.scss'
+import Img from './Img'
+import Button from './Button'
 
-const PageHeader = () => {
+const PageHeader = ({ introTitle, introDesc, introImg }) => {
   return (
     <Div100vh>
       <header className='min-h-screen'>
-        <div className='flex'>
-          <div className='basis-1/2'>hello</div>
-          <div className='basis-1/2'>hi</div>
-        </div>
+        <main className='flex h-full'>
+          <section className='basis-1/2'>
+            <Img
+              src={introImg?.url}
+              alt={introImg?.name}
+              sizes='100vw'
+              width='600'
+              height='600'
+              className='!w-full !h-full object-cover'
+            />
+          </section>
+          <section className='basis-1/2 bg-bg'>
+            <div className='flex flex-col justify-center h-full px-24 text-gray mt-16'>
+              <span className='w-32 h-[1px] bg-gray'></span>
+              <h1 className='text-gray text-5xl my-6 font-light leading-normal'>{introTitle}</h1>
+              <p className='text-gray text-lg mb-10 font-thin leading-relaxed'>{introDesc}</p>
+              <div>
+                <Button white className='inline-block'>
+                  Contact Me
+                </Button>
+              </div>
+            </div>
+          </section>
+        </main>
       </header>
     </Div100vh>
   )
 }
 
 export default PageHeader
+
+PageHeader.propTypes = {
+  introTitle: PropTypes.string,
+  introDesc: PropTypes.string,
+  introImg: PropTypes.object
+}
--- frontend/pages/about-me.js
+++ frontend/pages/about-me.js
@@ -1,13 +1,27 @@
+import { useQuery } from '@tanstack/react-query'
 import Layout from 'components/common/Layout'
 import PageHeader from 'components/common/PageHeader'
 import React from 'react'
 
-const aboutMePage = () => {
+const AboutMePage = () => {
+  const { data: introData } = useQuery(['about-me-intro'])
+  const {
+    data: {
+      attributes: {
+        title: introTitle,
+        description: introDesc,
+        image: {
+          data: { attributes: introImg }
+        }
+      }
+    }
+  } = { ...introData }
+
   return (
     <Layout title='About Me'>
-      <PageHeader />
+      <PageHeader introTitle={introTitle} introDesc={introDesc} introImg={introImg} />
     </Layout>
   )
 }
 
-export default aboutMePage
+export default AboutMePage
