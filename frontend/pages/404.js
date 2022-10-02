092606005d6f509decf3fa829961d72924ae6daf
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2022-10-02 23:04:35 +0600

    simplified global scss file

--- frontend/pages/404.js
+++ frontend/pages/404.js
@@ -1,48 +1,49 @@
 import { useRouter } from 'next/router'
 import React from 'react'
+import styles from 'styles/pages/error-page.module.scss'
 
 const Page404 = () => {
   const router = useRouter()
   return (
-    <div className='error-page'>
-      <section className='wrapper'>
-        <div className='container'>
-          <div id='scene' className='scene' data-hover-only='false'>
-            <div className='circle' data-depth='1.2'></div>
+    <div className={styles['error-page']}>
+      <section className={styles['wrapper']}>
+        <div className={styles['container']}>
+          <div id='scene' className={styles['scene']} data-hover-only='false'>
+            <div className={styles['circle']} data-depth='1.2'></div>
 
-            <div className='one' data-depth='0.9'>
-              <div className='content'>
-                <span className='piece'></span>
-                <span className='piece'></span>
-                <span className='piece'></span>
+            <div className={styles['one']} data-depth='0.9'>
+              <div className={styles['content']}>
+                <span className={styles['piece']}></span>
+                <span className={styles['piece']}></span>
+                <span className={styles['piece']}></span>
               </div>
             </div>
 
-            <div className='two' data-depth='0.60'>
-              <div className='content'>
-                <span className='piece'></span>
-                <span className='piece'></span>
-                <span className='piece'></span>
+            <div className={styles['two']} data-depth='0.60'>
+              <div className={styles['content']}>
+                <span className={styles['piece']}></span>
+                <span className={styles['piece']}></span>
+                <span className={styles['piece']}></span>
               </div>
             </div>
 
-            <div className='three' data-depth='0.40'>
-              <div className='content'>
-                <span className='piece'></span>
-                <span className='piece'></span>
-                <span className='piece'></span>
+            <div className={styles['three']} data-depth='0.40'>
+              <div className={styles['content']}>
+                <span className={styles['piece']}></span>
+                <span className={styles['piece']}></span>
+                <span className={styles['piece']}></span>
               </div>
             </div>
 
-            <p className='p404' data-depth='0.50'>
+            <p className={styles['p404']} data-depth='0.50'>
               404
             </p>
-            <p className='p404' data-depth='0.10'>
+            <p className={styles['p404']} data-depth='0.10'>
               404
             </p>
           </div>
 
-          <div className='text'>
+          <div className={styles['text']}>
             <article>
               <p>
                 Uh oh! Looks like you got lost. <br />
